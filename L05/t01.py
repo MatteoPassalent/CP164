4c356339762858cbd4d4f41660fc686b9acd09e4
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:05:20 -0400

    Create t01.py

---
+++ L05/t01.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2023-02-15"
+-------------------------------------------------------
+"""
+
+from functions import recurse
+
+ans = recurse(10, 11)
+print(ans)
