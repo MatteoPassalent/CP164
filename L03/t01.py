b20bcdedb7170b38a6b1d474094b09e8616c9527
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:02:45 -0400

    Create t01.py

---
+++ L03/t01.py
@@ -0,0 +1,21 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2023-01-31"
+-------------------------------------------------------
+"""
+
+from Queue_array import Queue
+
+q = Queue()
+
+q.insert(1)
+q.insert(2)
+q.insert(3)
+
+for i in q:
+    print(i)
