130f6700c667c60a54ece7a02d8b78c422f49026
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:39:09 -0400

    Create t01.py

---
+++ A07/t01.py
@@ -0,0 +1,16 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-11-18"
+-------------------------------------------------------
+"""
+from functions import list_factors
+
+num = int(input("Enter: "))
+answer = list_factors(num)
+
+print(answer)
