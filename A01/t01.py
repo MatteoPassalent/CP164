aa2c72621003a128e60b09bbd71db082755c8673
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:14:50 -0400

    Create t01.py

---
+++ A01/t01.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-10-02"
+-------------------------------------------------------
+"""
+print('The book title is, "Learn Python in 21 Days".')
+print("What's mine is mine, and what's yours is mine.")
+print('''"You have enemies? Good. That means you've stood up for something, sometime in your life." Winston Churchill''')  # noqa: E501
+print("""Three things cannot be long hidden:
+the sun,
+the moon,
+and the truth.""")
