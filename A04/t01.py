2d600e41c42c9aa3fbb1d320dee4d14318618917
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:36:14 -0400

    Create t01.py

---
+++ A04/t01.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-10-30"
+-------------------------------------------------------
+"""
+from functions import day_of_week
+
+day_number = int(input("Day of week: "))
+
+day = day_of_week(day_number)
+
+print(day)
