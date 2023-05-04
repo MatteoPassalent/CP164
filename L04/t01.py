1da71664bf4b44d6185e97cc4ac5c3526cbc9e28
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:04:13 -0400

    Create t01.py

---
+++ L04/t01.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2023-02-09"
+-------------------------------------------------------
+"""
+
+from Movie import Movie
+
+key = Movie('Juno', 2007, None, None, None)
+print(key)
