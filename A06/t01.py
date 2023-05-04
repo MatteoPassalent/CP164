ee61c9e0b13cbfccf6c3b0dd5af3c485cd2c6473
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:38:18 -0400

    Create t01.py

---
+++ A06/t01.py
@@ -0,0 +1,19 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-11-13"
+-------------------------------------------------------
+"""
+
+from functions import winner
+
+num = winner()
+
+num1 = num[0]
+num2 = num[1]
+
+print(f'{num1}, {num2}')
