6c1f1770837bf34b15b634cb24c455ddd80bcd2d
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:37:15 -0400

    Create t01.py

---
+++ A05/t01.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-11-06"
+-------------------------------------------------------
+"""
+from functions import factorial
+
+num = int(input("Enter a positive integer: "))
+
+answer = factorial(num)
+
+print(answer)
