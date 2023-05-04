218b456767e5471c6451ac1d5a5f63fe07e607a4
Matteo Edward Passalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-05-04 17:40:16 -0400

    Create t01.py

---
+++ A08/t01.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Matteo Passalent
+ID:      210597410
+Email:   pass7410@mylaurier.ca
+__updated__ = "2022-11-25"
+-------------------------------------------------------
+"""
+
+from functions import add_spaces
+
+string = input("Enter string: ")
+
+new_string = add_spaces(string)
+
+print(new_string)
